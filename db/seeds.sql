4a93ac3c390f6c30d6d215498fc36d3303b22595
Lakshdeep Bajwa <bajwals@live.com>
2020-03-18 15:23:52 -0700

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Ostrich Burger');
+INSERT INTO burgers (burger_name) VALUES ('Corona Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan Chickn Burger', true);
